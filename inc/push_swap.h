38a690d6f0d7ffc319197752dc85e442bb722977
Wincenty Lech <wincenty.lech@mercadolivre.com>
2021-07-11 14:13:44 -0300

    First commit

---
+++ inc/push_swap.h
@@ -0,0 +1,15 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <stdbool.h>
+# include "libft.h"
+# include "error.h"
+
+# ifndef NULL
+#  define NULL ((void *)0)
+# endif
+
+void ft_error(t_error error);
+
+#endif
