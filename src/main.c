e7d0a7ad2d0cd56593034441a750c1d55247328a
Wincenty Lech <wincenty.lech@mercadolivre.com>
2021-07-20 22:44:49 -0300

    Fixed Long Long number

--- src/main.c
+++ src/main.c
@@ -1,5 +1,19 @@
 #include "push_swap.h"
 
+bool has_more_than_11_char(int size, char const *nbr[])
+{
+	int i;
+
+	i = 0;
+	while(i < size)
+	{
+		if(ft_strlen(nbr[i + 1]) > 11)
+			return true;
+		i++;
+	}
+	return false;
+}
+
 void	sort(t_holder *holder)
 {
 	if (holder->a_stack->size <= 5)
@@ -14,6 +28,11 @@ int	main(int argc, char const *argv[])
 
 	if (argc < 2)
 		return (0);
+	if(has_more_than_11_char(argc - 1, argv))
+	{
+		ft_putstr_fd("Error\n", STDOUT_FILENO);
+		return (0);
+	}
 	holder = get_valid_holder(argc, argv);
 	if (holder != NULL)
 	{
